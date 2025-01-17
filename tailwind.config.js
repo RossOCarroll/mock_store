648cc6d35b074620ba644d095e55c9fad8c73f1f
RossOCarroll <124647528+RossOCarroll@users.noreply.github.com>
2025-01-16 17:41:25 -0800

    contact form

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,19 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './layout/*.liquid',
-    './sections/*.liquid',
-    './snippets/*.liquid',
-    './templates/customers/*.liquid',
-    './templates/*.liquid'
+    './**/*.liquid', // Match all Liquid files
+    './src/**/*.{js,jsx,ts,tsx}', // Match JS/TS/React files
+    './assets/*.css', // Match CSS files in the assets folder
   ],
+  darkMode: 'class', // Use 'media' if you prefer system-based dark mode
   theme: {
+    extend: {
+      height: {
+        '94': '22rem',
+      },
+    },
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
-}
-
+};
